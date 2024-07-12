0248f41df36f27745b994c9c6975dc8668eb3fda
Ebrahem-Outlook <EbrahemElbetar1@outlook.com>
2024-07-12 16:37:35 +0300

    V_7

---
+++ Application/Core/Messaging/ICommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace Application.Core.Messaging;
+
+public interface ICommand : IRequest
+{
+
+}
+
+public interface ICommand<out TResponse> : IRequest<TResponse>
+    where TResponse : notnull
+{
+
+}
---
+++ Application/Core/Messaging/IQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+
+namespace Application.Core.Messaging;
+
+public interface IQuery : IRequest
+{
+
+}
+
+
+public interface IQuery<out TResponse> : IRequest<TResponse>
+    where TResponse : notnull
+{
+
+}
